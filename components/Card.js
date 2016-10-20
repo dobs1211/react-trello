b3acabe2a1b4882950ccd991d32af81000e967c0
Olga Chavez <olgakatrin827@gmail.com>
2016-10-20 03:02:51 +0000

    final-with-test

---
+++ components/Card.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+//Card component, stateless, has items passed in via 'props' by parent, List
+var Card = function(props) {
+    return (
+            <div className="card">
+            <p>{props.title}</p>
+            <p>{props.content}</p>
+            </div>
+    );
+};
+
+module.exports = Card;
